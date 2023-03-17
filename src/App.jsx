2e819935e8424b2a24561708ebb5ef4b8bf59a6d
Mohammed Iliass Affani / <76954406+mohammediliassaffani@users.noreply.github.com>
2023-03-17 14:47:10 +0100

    create-formInput

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div className="app">
+      <form className="form"></form>
+    </div>
+  );
+};
+
+export default App;
---
+++ src/components/FormIbput.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "./forminput.css";
+
+const FormIbput = () => {
+  return <div className="formInput"></div>;
+};
+
+export default FormIbput;
